bf72ba068a87ecbbb35829a1f6869ca030aa547e
stani <nowakstas6@gmail.com>
2021-10-23 00:23:04 +0200

    comitt

--- Kolejka/Form1.Designer.cs
+++ Kolejka/Form1.Designer.cs
@@ -41,6 +41,26 @@ namespace Kolejka
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.dodaj_do_kolejki = new System.Windows.Forms.Button();
             this.usun_z_kolejki = new System.Windows.Forms.Button();
+            this.aktualny_pacjent = new System.Windows.Forms.Label();
+            this.wwww1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Imie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nazwa_badan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Data_badania = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nastepny_pacjent = new System.Windows.Forms.Label();
+            this.poprzedni_pacjent = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.data_badania_poprzedni_pacjent = new System.Windows.Forms.Label();
+            this.data_badania_wybrany_pacjent = new System.Windows.Forms.Label();
+            this.data_badania_nastepny_pacjent = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // nazwa_badan_text
@@ -65,6 +85,7 @@ namespace Kolejka
             this.podaj_imie.Name = "podaj_imie";
             this.podaj_imie.Size = new System.Drawing.Size(200, 23);
             this.podaj_imie.TabIndex = 2;
+            this.podaj_imie.TextChanged += new System.EventHandler(this.podaj_imie_TextChanged);
             // 
             // podaj_imie_text
             // 
@@ -113,6 +134,7 @@ namespace Kolejka
             // timer1
             // 
             this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // dodaj_do_kolejki
@@ -127,18 +149,202 @@ namespace Kolejka
             // 
             // usun_z_kolejki
             // 
-            this.usun_z_kolejki.Location = new System.Drawing.Point(254, 261);
+            this.usun_z_kolejki.Location = new System.Drawing.Point(373, 93);
             this.usun_z_kolejki.Name = "usun_z_kolejki";
             this.usun_z_kolejki.Size = new System.Drawing.Size(92, 23);
             this.usun_z_kolejki.TabIndex = 9;
             this.usun_z_kolejki.Text = "Usun z kolejki";
             this.usun_z_kolejki.UseVisualStyleBackColor = true;
+            this.usun_z_kolejki.Click += new System.EventHandler(this.usun_z_kolejki_Click);
+            // 
+            // aktualny_pacjent
+            // 
+            this.aktualny_pacjent.AutoSize = true;
+            this.aktualny_pacjent.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.aktualny_pacjent.Location = new System.Drawing.Point(588, 319);
+            this.aktualny_pacjent.Name = "aktualny_pacjent";
+            this.aktualny_pacjent.Size = new System.Drawing.Size(40, 15);
+            this.aktualny_pacjent.TabIndex = 10;
+            this.aktualny_pacjent.Text = "label1";
+            this.aktualny_pacjent.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // wwww1
+            // 
+            this.wwww1.Location = new System.Drawing.Point(63, 291);
+            this.wwww1.Name = "wwww1";
+            this.wwww1.Size = new System.Drawing.Size(234, 23);
+            this.wwww1.TabIndex = 11;
+            this.wwww1.Text = "button1";
+            this.wwww1.UseVisualStyleBackColor = true;
+            this.wwww1.Click += new System.EventHandler(this.poprzedni_pacjent_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(63, 184);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Imie,
+            this.Nazwa_badan,
+            this.Data_badania});
+            this.dataGridView1.Location = new System.Drawing.Point(588, 128);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(460, 150);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Imie
+            // 
+            this.Imie.HeaderText = "Imie";
+            this.Imie.Name = "Imie";
+            // 
+            // Nazwa_badan
+            // 
+            this.Nazwa_badan.HeaderText = "Nazwa_badan";
+            this.Nazwa_badan.Name = "Nazwa_badan";
+            // 
+            // Data_badania
+            // 
+            this.Data_badania.HeaderText = "Data_badania";
+            this.Data_badania.Name = "Data_badania";
+            // 
+            // nastepny_pacjent
+            // 
+            this.nastepny_pacjent.AutoSize = true;
+            this.nastepny_pacjent.Location = new System.Drawing.Point(588, 351);
+            this.nastepny_pacjent.Name = "nastepny_pacjent";
+            this.nastepny_pacjent.Size = new System.Drawing.Size(38, 15);
+            this.nastepny_pacjent.TabIndex = 15;
+            this.nastepny_pacjent.Text = "label1";
+            // 
+            // poprzedni_pacjent
+            // 
+            this.poprzedni_pacjent.AutoSize = true;
+            this.poprzedni_pacjent.Location = new System.Drawing.Point(588, 291);
+            this.poprzedni_pacjent.Name = "poprzedni_pacjent";
+            this.poprzedni_pacjent.Size = new System.Drawing.Size(38, 15);
+            this.poprzedni_pacjent.TabIndex = 16;
+            this.poprzedni_pacjent.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(480, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Poprzedni pacjent";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(480, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 15);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Wybrany pacjent";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(480, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 15);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Nastepny pacjent";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(660, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 15);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Data badania";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(660, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 15);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Data badania";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(660, 349);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 15);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Data badania";
+            // 
+            // data_badania_poprzedni_pacjent
+            // 
+            this.data_badania_poprzedni_pacjent.AutoSize = true;
+            this.data_badania_poprzedni_pacjent.Location = new System.Drawing.Point(752, 291);
+            this.data_badania_poprzedni_pacjent.Name = "data_badania_poprzedni_pacjent";
+            this.data_badania_poprzedni_pacjent.Size = new System.Drawing.Size(38, 15);
+            this.data_badania_poprzedni_pacjent.TabIndex = 23;
+            this.data_badania_poprzedni_pacjent.Text = "label8";
+            // 
+            // data_badania_wybrany_pacjent
+            // 
+            this.data_badania_wybrany_pacjent.AutoSize = true;
+            this.data_badania_wybrany_pacjent.Location = new System.Drawing.Point(752, 319);
+            this.data_badania_wybrany_pacjent.Name = "data_badania_wybrany_pacjent";
+            this.data_badania_wybrany_pacjent.Size = new System.Drawing.Size(38, 15);
+            this.data_badania_wybrany_pacjent.TabIndex = 24;
+            this.data_badania_wybrany_pacjent.Text = "label8";
+            // 
+            // data_badania_nastepny_pacjent
+            // 
+            this.data_badania_nastepny_pacjent.AutoSize = true;
+            this.data_badania_nastepny_pacjent.Location = new System.Drawing.Point(752, 348);
+            this.data_badania_nastepny_pacjent.Name = "data_badania_nastepny_pacjent";
+            this.data_badania_nastepny_pacjent.Size = new System.Drawing.Size(38, 15);
+            this.data_badania_nastepny_pacjent.TabIndex = 25;
+            this.data_badania_nastepny_pacjent.Text = "label8";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1093, 650);
+            this.Controls.Add(this.data_badania_nastepny_pacjent);
+            this.Controls.Add(this.data_badania_wybrany_pacjent);
+            this.Controls.Add(this.data_badania_poprzedni_pacjent);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.poprzedni_pacjent);
+            this.Controls.Add(this.nastepny_pacjent);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.wwww1);
+            this.Controls.Add(this.aktualny_pacjent);
             this.Controls.Add(this.usun_z_kolejki);
             this.Controls.Add(this.dodaj_do_kolejki);
             this.Controls.Add(this.Godzina);
@@ -152,6 +358,7 @@ namespace Kolejka
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,6 +377,25 @@ namespace Kolejka
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button dodaj_do_kolejki;
         private System.Windows.Forms.Button usun_z_kolejki;
+        private System.Windows.Forms.Label aktualny_pacjent;
+        private System.Windows.Forms.Button wwww1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Imie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nazwa_badan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data_badania;
+        private System.Windows.Forms.Label nastepny_pacjent;
+        private System.Windows.Forms.Label poprzedni_pacjent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label data_badania_poprzedni_pacjent;
+        private System.Windows.Forms.Label data_badania_wybrany_pacjent;
+        private System.Windows.Forms.Label data_badania_nastepny_pacjent;
     }
 }
 
--- Kolejka/Form1.cs
+++ Kolejka/Form1.cs
@@ -9,19 +9,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Kolejka
+
 {
     public partial class Form1 : Form
     {
+
+        public static Queue<string> kolejkapacjent;
+        //Queue kolejkapacjent = new Queue();
+        string nazwa;
+        string nazwa_badania;
+        DateTime dataa;
+        int liczba_dni;
+
         public Form1()
         {
             InitializeComponent();
-            //a
+            kolejkapacjent = new Queue<string>();
             
-
-
-
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -36,9 +42,13 @@ namespace Kolejka
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            
+
 
             data.Text = DateTime.Now.ToString("d/M/yyyy");
             Godzina.Text = DateTime.Now.ToString("t");
+            label1.Text = dataGridView1.Rows.Count.ToString();
+            
         }
 
         private void podaj_date_text_Click(object sender, EventArgs e)
@@ -48,9 +58,110 @@ namespace Kolejka
 
         private void dodaj_do_kolejki_Click(object sender, EventArgs e)
         {
-            Queue kolejkapacjent = new Queue();
-            kolejkapacjent.Enqueue(podaj_imie);
-            //kolejkapacjent.Count;
+            nazwa = podaj_imie.Text;
+            nazwa_badania = wprowadz_nazwe_badan.Text;
+            dataa = wybierz_date.Value.Date;
+            //kolejkapacjent.Enqueue(podaj_imie.ToString());
+
+            //aktualny_pacjent.Text = kolejkapacjent.Peek();
+
+
+            //foreach (Object obj in kolejkapacjent)
+           // { 
+            //  aktualny_pacjent.Text = (obj.ToString());
+           // }
+
+           // int i = kolejkapacjent.Count;
+            
+
+           // label1.Text = i.ToString();
+
+
+            dataGridView1.Rows.Add(nazwa,nazwa_badania,dataa );
+
+
+        }
+
+
+        private void usun_z_kolejki_Click(object sender, EventArgs e)
+        {
+            //kolejkapacjent.Dequeue();
+
+            if (this.dataGridView1.SelectedRows.Count > 0 &&
+            this.dataGridView1.SelectedRows[0].Index !=
+            this.dataGridView1.Rows.Count - 1)
+            {
+
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void podaj_imie_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void poprzedni_pacjent_Click(object sender, EventArgs e)
+        {
+
+            kolejkapacjent.Dequeue();
+            foreach (Object obj in kolejkapacjent)
+            {
+                aktualny_pacjent.Text = (obj.ToString());
+            }
+
+            
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentCell.RowIndex >= 1 )
+            {
+                poprzedni_pacjent.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index - 1].Cells[0].Value.ToString();
+                
+
+                liczba_dni = (DateTime.Now - Convert.ToDateTime(dataGridView1.Rows[dataGridView1.CurrentRow.Index - 1].Cells[2].Value)).Days;
+
+                if (liczba_dni < 0)
+                {
+                    //ustawic kursywy pogrubiona w labelce 
+                }else if(liczba_dni == 0)
+                {
+                    data_badania_poprzedni_pacjent.Text = ("Dzisiaj");
+                    //na czerwono dzisiaj
+                }
+                else(liczba_dni > 0){
+                    data_badania_poprzedni_pacjent.Text = ("badanie odbylo sie ", liczba_dni.ToString(), "dni temu");
+                    //kursywa
+                }
+
+                data_badania_poprzedni_pacjent.Text = liczba_dni.ToString();
+
+            }
+            else
+            {
+                poprzedni_pacjent.Text = ("Brak");
+            }
+            
+            aktualny_pacjent.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString();
+            
+            
+            if (dataGridView1.CurrentCell.RowIndex < dataGridView1.Rows.Count - 2)
+            {
+                nastepny_pacjent.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index + 1].Cells[0].Value.ToString();
+            }
+            else
+            {
+                nastepny_pacjent.Text = ("Brak");
+            }
         }
     }
 }
